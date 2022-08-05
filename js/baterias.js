8ea08a398f53f3bb64c1654f82433231c7fd676f
Saulo Costa <saulotarsobc@gmail.com>
2022-08-05 08:33:11 -0300

    Update baterias.js

--- js/baterias.js
+++ js/baterias.js
@@ -2,7 +2,7 @@ let bat_wrap = document.querySelector('#bat_wrap');
 
 let bar = '<div class="bar"></div>';
 
-var nivel = 1; /* NIVEL DA BATERIA RANGE 1 - 5 */
+var nivel = 3; /* NIVEL DA BATERIA RANGE 1 - 5 */
 count = 0;
 
 while (count < nivel) {
